dd19df61b53266150221e8a3bdf91f83fb0b86a2
LoganPenguino <loganseguin11@gmail.com>
2025-01-17 12:17:46 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-const add = document.getElementById("add");
-const amount = document.getElementById("amount");
-const news = document.getElementById("news");
-
-add.onclick = function(){
-    count++;
-    amount.textContent = count;
-};
