5b44c25d00c711084c55d11046e218b723a12188
LoganPenguino <loganseguin11@gmail.com>
2025-02-12 09:12:48 -0500

    hhfhf

---
+++ js/a320.js
@@ -0,0 +1,7 @@
+const a320 = document.getElementById("a320");
+const message = document.getElementById("message");
+
+a320.onclick = function(){
+    message.textContent = "https://realfishyking.itch.io/onelifelizard";
+    setTimeout("message.textContent = ' '"))
+}
