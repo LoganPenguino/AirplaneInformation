44b39b117c4668498d952ce9f77e90f4f37aea82
LoganPenguino <loganseguin11@gmail.com>
2025-02-12 13:03:04 -0500

    jh

--- js/a320.js
+++
@@ -1,7 +0,0 @@
-const a320 = document.getElementById("a320");
-const message = document.getElementById("message");
-
-a320.onclick = function(){
-    message.textContent = "https://realfishyking.itch.io/onelifelizard";
-    setTimeout("message.textContent = ' '", 2000);
-}
